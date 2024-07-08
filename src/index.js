5911b21c81b0da35d239b7ffcfc9c1bcb6900bcd
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-07-08 11:38:23 +0300

    event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    func(document.getElementById("new-task-description").value)
+    form.reset()
+})
+})
+
+function func(todo){
+  let par = document.createElement('p')
+  let bt = document.createElement('button')
+  bt.addEventListener("click", del)
+  par.textContent = `${todo} `
+  bt.textContent = "X"
+  par.appendChild(bt)
+  document.querySelector("#tasks").appendChild(par)
+  console.log(par)
+}
+
+function del(e) {
+  e.target.parentNode.remove()
+}
+
+
